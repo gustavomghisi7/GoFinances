493be076cf2d1ebf47df49045fc3ca4fc44a38ec
Gustavo Martins Ghisi <guga-ghisi@hotmail.com>
2021-07-05 16:54:54 -0300

    dashboard

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
+
 import { HighlightCard } from '../../components/HighlightCard';
+import {
+    TransactionCard,
+    TransactionCardProps
+} from '../../components/TransactionCard';
 
 import {
     Container,
@@ -11,20 +16,63 @@ import {
     UserGreeting,
     UserName,
     Icon,
-    HighlightCards
+    HighlightCards,
+    Transactions,
+    Title,
+    TransactionList
 } from './styles'
 
+export interface DataListProps extends TransactionCardProps {
+    id: string;
+}
+
 export function Dashboard(){
+    const data: DataListProps[] = [
+        {
+            id: '1',
+            type: 'positive',
+            title: "Desenvolvimento de Site",
+            amount: "R$ 12.000,00",
+            category: {
+                name: 'Vendas',
+                icon: 'dollar-sign'
+            },
+            date: "13/04/2020"
+        },
+        {
+            id: '2',
+            type: 'negative',
+            title: "Hamburgueria Pizzy",
+            amount: "R$ 59,00",
+            category: {
+                name: 'Alimentação',
+                icon: 'coffee'
+            },
+            date: "10/04/2020"
+        },
+        {
+            id: '3',
+            type: 'negative',
+            title: "Aluguel do Apartamento",
+            amount: "R$ 1.200,00",
+            category: {
+                name: 'Casa',
+                icon: 'shopping-bag'
+            },
+            date: "27/03/2020"
+        },
+    ];
+
     return (
         <Container>
             <Header>
                 <UserWrapper>
                     <UserInfo>
                         <Photo
-                            source={{ uri: 'https://avatars.githubusercontent.com/u/56934686?v=4'}} />
+                            source={{ uri: 'https://avatars.githubusercontent.com/u/49030804?v=4'}} />
                         <User>
                             <UserGreeting>Olá</UserGreeting>
-                            <UserName>Gustavo</UserName>
+                            <UserName>Rodrigo</UserName>
                         </User>
                     </UserInfo>
                     
@@ -33,10 +81,37 @@ export function Dashboard(){
             </Header>
             
             <HighlightCards>
-                <HighlightCard />
-                <HighlightCard />
-                <HighlightCard />
-            </HighlightCards>        
+                <HighlightCard
+                    type="up"
+                    title="Entradas"
+                    amount="R$ 17.400,00"
+                    lastTransaction="Última entrada dia 13 de Abril"
+                />
+
+                <HighlightCard
+                    type="down"
+                    title="Saídas"
+                    amount="R$ 17.400,00"
+                    lastTransaction="Última saída dia 03 de Abril"
+                />
+
+                <HighlightCard
+                    type="total"
+                    title="Total"
+                    amount="R$ 16.141,00"
+                    lastTransaction="01 à 16 de Abril"
+                />
+            </HighlightCards>   
+
+            <Transactions>
+                <Title>Listagem</Title>
+
+                <TransactionList
+                    data={data}
+                    keyExtractor={item => item.id}
+                    renderItem={({ item }) => <TransactionCard data={item} /> }
+                />
+            </Transactions>     
         </Container>
     )
 }
