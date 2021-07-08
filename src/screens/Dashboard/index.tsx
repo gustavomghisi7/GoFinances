845daf14b57a2d7e62d1a39556f49df9d0a581c2
Gustavo Martins Ghisi <guga-ghisi@hotmail.com>
2021-07-08 17:35:42 -0300

    navigation

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -19,7 +19,8 @@ import {
     HighlightCards,
     Transactions,
     Title,
-    TransactionList
+    TransactionList,
+    LogoutButton
 } from './styles'
 
 export interface DataListProps extends TransactionCardProps {
@@ -76,7 +77,10 @@ export function Dashboard(){
                         </User>
                     </UserInfo>
                     
-                    <Icon name="power"/>
+                    <LogoutButton onPress={ () => {} }>
+                        <Icon name="power"/>
+                    </LogoutButton>
+                    
                 </UserWrapper>
             </Header>
             
